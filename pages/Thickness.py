02fe121aca309060cf0e7c53e9ed24907082015c
tle1223 <gungame00@hotmail.com>
2026-05-08 10:02:00 +0700

    Update Thickness.py

--- pages/Thickness.py
+++ pages/Thickness.py
@@ -14,7 +14,7 @@ if "company" not in st.session_state:
 company = st.selectbox(
     "เลือกบริษัท",
     ["บริษัท PAN", "บริษัท MIKI"],
-    index=["บริษัท PAn", "บริษัท MIKI"].index(st.session_state["company"])
+    index=["บริษัท PAN", "บริษัท MIKI"].index(st.session_state["company"])
 )
 st.session_state["company"] = company
 
